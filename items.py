039945b1a80cfcc0067043efa3008768ac0a6227
Muustafa11 <85533548+Muustafa11@users.noreply.github.com>
2024-07-01 14:23:22 +0300

    Delete items.py

--- items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class AmazonItem(scrapy.Item):
-    id = scrapy.Field()
-    brand_name = scrapy.Field()
-    brief = scrapy.Field()
-    price = scrapy.Field()
-    product_url = scrapy.Field()
-    image_url = scrapy.Field()
